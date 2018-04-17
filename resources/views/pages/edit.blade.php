d89d9805fc5f3fa052826c10b3719c294db43f17
lenovo <chicumihai@gmail.com>
2018-04-17 13:41:42 +0300

    Working edit

---
+++ resources/views/pages/edit.blade.php
@@ -0,0 +1,58 @@
+<html>
+
+@include("layouts.navbar")<br><br>
+<body>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-8 col-md-offset-2">
+
+            <h1>Edit Page</h1>
+            <form action="{{url("/pages/".$page->id)}}" method="POST" enctype="multipart/form-data">
+                {{csrf_field()}}
+                {{method_field('PATCH')}}
+                @if(Session::has('message'))
+                    <div class="alert alert-success">
+                        {{ Session::get('message') }}
+                    </div>
+                @endif
+
+
+                <div class="form-group">
+                    <label for="title">Title <span class="require">*</span></label>
+                    <input type="text" class="form-control" name="title" placeholder="{{$page->title}}"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <span class="require">*</span> <textarea rows="5" class="form-control" name="body" placeholder="{{$page->body}}"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlFile1">Upload picture</label>
+                    <input type="file" class="form-control-file" name="img">
+                </div>
+
+                <div class="form-group">
+                    <p><span class="require">*</span> - required fields</p>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">
+                        Update
+                    </button>
+                    <button class="btn btn-default">
+                        Cancel
+                    </button>
+                </div>
+
+
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+@include("layouts.footer")
+
+
+</html>
