aa533ab9a56668e957afa8f9f4df356b8e246535
ChicuMihai <33778619+ChicuMihai@users.noreply.github.com>
2018-04-20 08:04:27 +0300

    Added View composer for navbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Page;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        view()->composer('layouts.navbar',function($view){
+        $pages=Page::get();
+        $view->with('pages',$pages);
+        });
     }
     /**
      * Register any application services.
